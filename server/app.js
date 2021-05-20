6196d543c87da54dc43daaa011ae3f101c284817
erickes5trada <erick.e.senado@gmail.com>
2021-05-19 23:13:39 -0500

    added plugin & middlewares into app

--- server/app.js
+++ server/app.js
@@ -12,6 +12,7 @@ import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.dev.config';
+import webpackDevConfig from '../webpack.dev.config';
 
 // Consultamos el modo en el que se esta ejecutando el programa
 const env = process.env.NODE_ENV || 'development';
@@ -19,6 +20,30 @@ const env = process.env.NODE_ENV || 'development';
 // Se crea la aplicación de express
 var app = express();
 
+// verificando el modo de ejecución de la aplicación
+if(env === 'development'){
+  console.log('executing in development mode: Webpack hot reloading');
+  // Paso 1: agregando la ruta del hot module reloading
+  // reload=true: habilita la recarga del frontend cuando hay cambios en el código
+  // timeout: tiempo de recarga y espera  
+  webpackConfig.entry = ['wbepack-hot-middleware/client?reload=true&timeout=1000',
+  webpackConfig.entry];
+
+  // Paso 2: agregando el plugin
+  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin);
+
+  // Paso 3: creando compilador de webpack
+  const compiler = webpack(webpackConfig);
+
+  // Paso 4: Agregando el middleware a la cadena de middlewares de la aplicación
+  app.use(webpackDevMiddleware(compiler,
+    {
+      publicPath: webpackDevConfig.output.publicPath
+    }));
+
+  // Paso 5: agregando webpack hot middleware
+  app.use(webpackHotMiddleware(compiler));
+}
 
 
 // view engine setup
