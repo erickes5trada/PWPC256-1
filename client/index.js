32cb9a0725df177bd8ccf67c7506c78e6701a397
erickes5trada <erick.e.senado@gmail.com>
2021-05-24 21:53:14 -0500

    instaled aribnb style code and formatted index.js

--- client/index.js
+++ client/index.js
@@ -1,27 +1,26 @@
-import './stylesheets/style.css'
-import './stylesheets/myStyles.css'
+import './stylesheets/style.css';
+import './stylesheets/myStyles.css';
 
-console.log("Webpack funcionando!!");
+console.log('Webpack funcionando!!');
 
-let show = (m = "hola") => {
+const show = (m = 'hola') => {
   alert(m);
-}
+};
 
 show();
 
-function resolveAfter2Seconds(){
-  return new Promise(resolve =>{
-    setTimeout(() =>{
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
       resolve('resolve');
-    },2000);
+    }, 2000);
   });
 }
 
-async function asyncCall(){
-  console.log("Calling an async funcion");
+async function asyncCall() {
+  console.log('Calling an async funcion');
   const result = await resolveAfter2Seconds();
   console.log(result);
 }
 
 asyncCall();
-
